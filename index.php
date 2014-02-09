af5ec10bf0690df2cd36d16e6c7a7657dfb0fdf3
kouhei0725 <feel-the-wind.sakura@hotmail.co.jp>
2014-02-09 22:06:34 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+?>
+
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>メメント☆モリ</title>
+</head>
+<body>
+	小野寺prpr
+	<img src = "./img/nighit.jpg" WIDTH="80" HEIGHT="150">
+</body>
+
+</html>
